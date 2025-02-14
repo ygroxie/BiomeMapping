9c7669b6b6211205c6bb614f75caeb185ab20e12
Ygroxie <ygroxie34@gmail.com>
2025-02-14 03:11:23 +0100

    change

--- src/main/java/com/pieterdebot/biomemapping/BiomeMappingAPI.java
+++ src/main/java/com/pieterdebot/biomemapping/BiomeMappingAPI.java
@@ -56,24 +56,6 @@ public class BiomeMappingAPI implements VersionWrapper{
         switch (version){
             case v1_8_R3:
                 return new Wrapper_1_8_R3();
-            case v1_9_R1:
-                return new Wrapper_1_9_R1();
-            case v1_9_R2:
-                return new Wrapper_1_9_R2();
-            case v1_10_R1:
-                return new Wrapper_1_10_R1();
-            case v1_11_R1:
-                return new Wrapper_1_11_R1();
-            case v1_12_R1:
-                return new Wrapper_1_12_R1();
-            case v1_13_R1:
-                return new Wrapper_1_13_R1();
-            case v1_13_R2:
-                return new Wrapper_1_13_R2();
-            case v1_14_R1:
-                return new Wrapper_1_14_R1();
-            case v1_15_R1:
-                return new Wrapper_1_15_R1();
         }
 
         throw new RuntimeException("Version " + version + " is not supported!");
